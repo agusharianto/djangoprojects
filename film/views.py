a2eaa21b0e5ae9bba98a00f6ae313266be5156ae
bac0ke <agusharianto848@gmail.com>
2021-04-25 23:30:03 +0800

    update project

--- film/views.py
+++ film/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	context = {
+		'title':'Film Terbaru'
+	}
+	return render(request, 'film/index.html', context)
